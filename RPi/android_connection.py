2e1a9c4456da930123701b954f0331aa3ff712d8
kghite <kghite@gmail.com>
2015-10-27 17:50:11 +0000

    Setting up RPi folder, initial structure and docs

---
+++ RPi/android_connection.py
@@ -0,0 +1,7 @@
+!#/usr/local/bin/python
+
+"""
+android_connection.py
+Description: Provide Android comm functions for lazer maze system
+Edited: 10/27/15
+"""
---
+++ RPi/input_control.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+
+"""
+input_control.pi
+Description: Main control of lazer maze and GPIO
+Edited: 10/26/15
+"""
+
+import RPI.GPIO as GPIO
+from android_connection import *
+
+print "Running"
+
