90c65799817b25b28d6956d666cdcc7b053a8b51
kghite <kghite@gmail.com>
2015-10-28 04:17:44 +0000

    adding basic python structure and flask integration in RPi

--- RPi/scripts/android_connection.py
+++ RPi/scripts/android_connection.py
@@ -1,4 +1,4 @@
-!#/usr/local/bin/python
+#!/usr/local/bin/python
 
 """
 android_connection.py
--- RPi/scripts/input_control.py
+++ RPi/scripts/input_control.py
@@ -6,8 +6,40 @@ Description: Main control of lazer maze and GPIO
 Edited: 10/26/15
 """
 
-import RPI.GPIO as GPIO
+### Setup ###
+import RPi.GPIO as GPIO
 from android_connection import *
 
-print "Running"
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+# Servos
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+# Photocells
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+GPIO.setup(8, GPIO.IN)
+GPIO.setup(7, GPIO.IN)
+# Lasers
+GPIO.setup(10, GPIO.out)
 
+### Control ###
+# Main Loop
+while(1):
+	# Get Photocell input
+	pc1 = GPIO.input(4)
+	pc2 = GPIO.input(17)
+	pc3 = GPIO.input(21)
+	pc4 = GPIO.input(22)
+	PC = [pc1, pc2, pc3, pc4]
+	# Get Android input
+		
+	# Determine if a beam was broken
+	for pc in PC:
+		if pc == False:
+			# Set off alarm
+			print "Beam Broken"
+	# Update Servo positions 
+	
