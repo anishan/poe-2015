20b852776ed813051c36b68376dd168e910ef554
kghite <kghite@gmail.com>
2015-11-04 04:13:47 +0000

    adding test structure for photodiodes and lasers

--- RPi/scripts/test_box.py
+++ RPi/scripts/test_box.py
@@ -5,16 +5,32 @@ Edited: 11/3/15
 
 import os, time, sys
 from pygame import mixer
+import RPi.GPIO as GPIO
 
+
+# Define GPIO and pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+laser_pin = 2
+pd1_pin = 3
 
 def sound_test(wav_file):
 	os.system('omxplayer -o local ' + wav_file)
 
 def laser_test():
-	pass
+	GPIO.setup(laser_pin, GPIO.OUT)
+	GPIO.output(laser_pin, GPIO.HIGH)
+	print 'Lasers On'
+	time.sleep(20)
 
 def diode_test():
-	pass
+	GPIO.setup(pd1_pin, GPIO.IN)
+	while(1):
+		pd1 = GPIO.input(pd1_pin)
+		print pd1
+		time.sleep(1)
 
 
-sound_test('sad_trombone.wav')
+#sound_test('sad_trombone.wav')
+#laser_test()
+diode_test()
