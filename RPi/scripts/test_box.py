4f65d148d1ac307261e476d03cf07db3132c269f
kghite <kghite@gmail.com>
2015-11-04 03:36:53 +0000

    added sound test code

--- RPi/scripts/test_box.py
+++ RPi/scripts/test_box.py
@@ -3,11 +3,18 @@ Test File for Maze Prototypes
 Edited: 11/3/15
 """
 
+import os, time, sys
+from pygame import mixer
+
+
+def sound_test(wav_file):
+	os.system('omxplayer -o local ' + wav_file)
+
 def laser_test():
 	pass
 
 def diode_test():
 	pass
 
-def sound_test():
-	pass
+
+sound_test('sad_trombone.wav')
