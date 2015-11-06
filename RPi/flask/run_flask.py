d95f1e4f2d7620798cb1af83bd07d266dff10bd8
kghite <kghite@gmail.com>
2015-11-06 03:14:06 -0500

    created flask interface for countdown, timer, and stop button

---
+++ RPi/flask/run_flask.py
@@ -0,0 +1,37 @@
+import time
+from flask import Flask, render_template
+app = Flask(__name__)
+
+start_time = time.time()
+
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+
+@app.route('/countDownPage1')
+def countDown1():
+	return render_template('countDownPage1.html')
+
+@app.route('/countDownPage2')
+def countDown2():
+	return render_template('countDownPage2.html')
+
+@app.route('/countDownPage3')
+def countDown3():
+	return render_template('countDownPage3.html')
+
+@app.route('/game')
+def game():
+	game_time = time.time() - start_time - 7
+	game_time = round(game_time, 2)
+	return render_template('game.html', game_time=game_time)
+
+@app.route('/score')
+def score():
+	game_time = time.time() - start_time - 7
+	return render_template('score.html', game_time=game_time)
+
+
+if __name__ == "__main__":
+	app.run()
--- RPi/scripts/test_box.py
+++ RPi/scripts/test_box.py
@@ -3,34 +3,12 @@ Test File for Maze Prototypes
 Edited: 11/3/15
 """
 
-import os, time, sys
-from pygame import mixer
-import RPi.GPIO as GPIO
-
-
-# Define GPIO and pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-laser_pin = 2
-pd1_pin = 3
-
-def sound_test(wav_file):
-	os.system('omxplayer -o local ' + wav_file)
 
 def laser_test():
-	GPIO.setup(laser_pin, GPIO.OUT)
-	GPIO.output(laser_pin, GPIO.HIGH)
-	print 'Lasers On'
-	time.sleep(20)
+	pass
 
 def diode_test():
-	GPIO.setup(pd1_pin, GPIO.IN)
-	while(1):
-		pd1 = GPIO.input(pd1_pin)
-		print pd1
-		time.sleep(1)
+	pass
 
-
-#sound_test('sad_trombone.wav')
-#laser_test()
-diode_test()
+def sound_test():
+	pass
