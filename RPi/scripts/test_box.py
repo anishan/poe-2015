454575cf045f3b73043de9ee33ae30e9d6d369ac
kghite <kghite@gmail.com>
2015-12-01 18:58:16 +0000

    RPi test box code

--- RPi/scripts/test_box.py
+++ RPi/scripts/test_box.py
@@ -2,13 +2,28 @@
 Test File for Maze Prototypes
 Edited: 11/3/15
 """
+import os, time, system
+from pygame import mixer
+import RPi.GPIO as GPIO
 
+#Define GPIO and pins
+GPIO.setmode(GPIO.BCM)
+laser_pin = 2
+pd1_pin = 2
+GPIO.setup(pd1_pin, GPIO.IN)
 
-def laser_test():
-	pass
+def sound_test(wav_file):
+	os.system('omxplayer -o local '+ wav_file)
 
 def diode_test():
-	pass
+	pd1 = GPIO.input(pd1_pin)
+	return pd1
 
-def sound_test():
-	pass
+def game_test():
+	diode1 = diode_test()
+	while diode1 == 1:
+		diode1 = diosde_test()
+		print diode1
+	sound_test('sad_trombone.wav')
+
+game_test()
