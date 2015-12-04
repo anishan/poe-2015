4e0d9e1badf9511e7ffa6edf25849c4edba75cab
kghite <kghite@gmail.com>
2015-12-03 22:58:35 -0500

    linked pages up through game.html

--- RPi/flask/run_flask.py
+++ RPi/flask/run_flask.py
@@ -11,9 +11,21 @@ start_time = time.time()
 def main():
 	return render_template('index.html')
 
-# @app.route('/countDownPage1')
-# def countDown1():
-# 	return render_template('countDownPage1.html')
+@app.route('/countDown')
+def countDown():
+	return render_template('countDown.html')
+
+@app.route('/game')
+def game():
+	return render_template('game.html')
+
+@app.route('/saveTime')
+def saveTime():
+	return render_template('saveTime.html')
+
+@app.route('/score')
+def score():
+	return render_template('score.html')
 
 # @app.route('/countDownPage2')
 # def countDown2():
@@ -23,16 +35,16 @@ def main():
 # def countDown3():
 # 	return render_template('countDownPage3.html')
 
-@app.route('/game')
-def game():
-	game_time = time.time() - start_time - 7
-	game_time = round(game_time, 2)
-	return render_template('game.html', game_time=game_time)
+# @app.route('/game')
+# def game():
+# 	game_time = time.time() - start_time - 7
+# 	game_time = round(game_time, 2)
+# 	return render_template('game.html', game_time=game_time)
 
-@app.route('/score')
-def score():
-	game_time = time.time() - start_time - 7
-	return render_template('score.html', game_time=game_time)
+# @app.route('/score')
+# def score():
+# 	game_time = time.time() - start_time - 7
+# 	return render_template('score.html', game_time=game_time)
 
 # @app.route('/', methods=['POST'])
 # def my_form_post():
