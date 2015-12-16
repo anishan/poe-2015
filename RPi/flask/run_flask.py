cb452f9f3e8db0f465aad536bf664c2b74b8a0c3
kghite <kghite@gmail.com>
2015-12-16 00:47:31 +0000

    add rpi code to run_flask.py

--- RPi/flask/run_flask.py
+++ RPi/flask/run_flask.py
@@ -1,12 +1,46 @@
+import os, time
 from flask import Flask, render_template, request
 # from flask import *
 app = Flask(__name__)
 import sqlite3
 from flask import g
 import json
+import RPi.GPIO as GPIO
 
+# User information from web gui
 endTime = 0
+foul_time = 0
 userName = ""
+
+# Define GPIO pins 
+arduino_reset_pin = 12
+servo_control_pin = 21
+pd1_pin = 6
+pd2_pin = 13
+pd3_pin = 19
+pd4_pin = 26
+# Initialize GPIO i/o
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(arduino_reset_pin, GPIO.OUT)
+GPIO.setup(servo_control_pin, GPIO.OUT)
+GPIO.setup(pd1_pin, GPIO.IN)
+GPIO.setup(pd2_pin, GPIO.IN)
+GPIO.setup(pd3_pin, GPIO.IN)
+GPIO.setup(pd4_pin, GPIO.IN)
+
+def game_state():
+	# Check for broken lazers
+	p1 = GPIO.input(pd1_pin)
+	p2 = GPIO.input(pd2_pin)
+	p3 = GPIO.input(pd3_pin)
+	p4 = GPIO.input(pd4_pin)
+	pds = [p1, p2, p3, p4]
+	if 1 in pds:
+		# Play sad trombone sound
+		os.system('omxplayer -o local sad_trombone.wav')
+		# Add time to the score
+		foul_time += 30
 
 @app.route('/')
 def main():
@@ -14,6 +48,9 @@ def main():
 
 @app.route('/countDown')
 def countDown():
+	# Reset the Arduino for calibration
+	GPIO.output(arduino_reset_pin, GPIO.LOW)
+	GPIO.output(arduino_reset_pin, GPIO.HIGH)
 	return render_template('countDown.html')
 
 @app.route('/game')
@@ -36,7 +73,8 @@ def score():
 		userName = userName.upper()
 		print userName
 		global endTime
-		to_insert = (userName,endTime,) # because this needs to be a tuple
+		global foul_time
+		to_insert = (userName,endTime + foul_time,) # because this needs to be a tuple
 		conn = sqlite3.connect('gameTimeDatabase.db')
 		c = conn.cursor()
 		c.execute("INSERT INTO times VALUES (?,?)", to_insert)
